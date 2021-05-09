21ece9510dc552f3bc2d1612247541a922cff425
Gazala malik <57172932+gazalamalik@users.noreply.github.com>
2021-05-09 14:39:28 -0400

    chang

--- Online_mcq_site/dbConn.php
+++ Online_mcq_site/dbConn.php
@@ -401,21 +401,8 @@ public function OptionTableList($question_id){
 
 	 }
 
-	 public function query_InsertQuestion($sql1) {
-		$ret = pg_query($this->db, $sql1);
-		$row = pg_num_rows($ret);
+	 
 
-		
-		return $row;
-	
-	}
-
-
-
-
-
-
-	//tanuja
 
 
 			
@@ -426,136 +413,14 @@ public function query2($sql) {
        return $ret;
 }
 
-public function fetch_row($result) {
-    $ret = pg_fetch_row($result);
-   return $ret;
-}
-public function num_rows($result) {
-    $ret = pg_num_rows($result);
-   return $ret;
-}
-public function fetch_all($result) {
-    $ret = pg_fetch_all($result);
-   return $ret;
-}
 
-public function insert($table,$params=array()){
-    if($this->tableexists($table)){
-        $table_columns=implode(',',array_keys($params));
-        $table_values=implode(',',($params));
-       $sql2="INSERT INTO $table ($table_columns) VALUES ($table_values);";
-       if(pg_query($this->db,$sql2)){
-            echo 'row inserted';
-            return true;
-       }
-       else{
-           echo pg_last_error($this->db);
-           return false;
-       }
-    }
-  else{
-      return false;
-  }
+
        
- }
-
-
-   public function update($table,$params=array(),$where=null){
-    if($this->tableexists($table))
-    {
-        $args=array();
-        foreach($params as $key =>$value){
-            $args[]="$key='$value'";
-        }
-
-        $sql2="UPDATE $table SET ".implode(',',$args);
-        if($where!=null){
-            $sql2 .=" WHERE $where";
-        }
-        if(pg_query($this->db,$sql2)){
-            echo 'data updated';
-            return true;
-       }
-       else{
-           echo pg_last_error($this->db);
-           return false;
-       }
-    }
-
-   }
-
-   public function delete($table,$where=null){
-    if($this->tableexists($table)){
-            $sql2="DELETE FROM $table";
-            if($where!=null){
-                $sql2 .=" WHERE $where";
-            }
-            if(pg_query($this->db,$sql2)){
-                echo 'data deleted';
-                return true;
-           }
-           else{
-               echo pg_last_error($this->db);
-               return false;
-           }
-    }
-    else{
-        return false;
-    }
-   }
-
-   public function select($table,$rows="*",$join=null,$where=null,$order=null,$limit=null){
-    if($this->tableexists($table)){
-        $sql="SELECT $rows FROM $table";
-        if($join!=null)
-        {
-            $sql .=" JOIN $join";
-        }
-        if($where!=null)
-        {
-            $sql .=" WHERE $where";
-        }
-        if($order!=null)
-        {
-            $sql .=" ORDER BY $order";
-        }
-        if($limit!=null)
-        {
-            $sql .=" LIMIT 0,$limit";
-        }
-        echo $sql;
-        $query=pg_query($this->db,$sql);
-        if($query){
-            $this->result=pg_fetch_all($query);
  
-            return true;
- 
-        }else{
-            echo array_push($this->result,pg_last_error($this->db));
-             return false;
-        }
- 
-    }
- 
-    else{
-        return false;
-    }
 
 
-   }
-   public function sql($sql2){
-         $query=pg_query($this->db,$sql2);
-       if($query){
-           $this->result=pg_fetch_all($query);
-
-           return true;
-
-       }else{
-           echo array_push($this->result,pg_last_error($this->db));
-            return false;
-       }
-
-   }
+  
+  
 
 private function tableexists($table){
 
